79f05da8a9b8cc8576562d2736287524fc0a1b27
Liam Baxter <liam@coffee-design.co.uk>
2019-07-13 16:16:42 +0100

    fething data via axios

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,33 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 6c273cebce0473d11dc06bcddf2175aeec32470e7d236072c878f7bd981a025c"
+      }
+    });
+    // older way of call back, instead using async await!
+    //   .then(response => {
+    //     // callbacck
+    //     console.log(response.data.results);
+    //   });
+    console.log(response.data.results);
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
